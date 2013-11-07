8b671c4dad719181562bc1218da48f8e383e2422
Tero Suhonen <teros@ubuntu.(none)>
2013-11-07 13:36:41 -0800

    added checkboxes

--- controllers/default.py
+++ controllers/default.py
@@ -6,43 +6,39 @@ def index():
     #select all entries
     rows = db(db.games.id > 0).select()
     #create checkboxes to select the game odds
-    #selForm = FORM()
-    #for idx in range(len(rows)*3):
-        #selForm.append(INPUT(_type='checkbox'))
     selForm = SQLFORM(db.checkBox)
-    #selForm.append(INPUT(_type="submit", _value="OK", _name="okButton"))
-    #get selected odds
-    oddsSelected = []
     numOfSafes = 0
-    numOfPartial = 0
-    numOfFull = 0
+    numOfPartials = 0
+    numOfFulls = 0 
+    checkBoxValues = []
     if selForm.validate(keepvalues=True): #process().accepted:
-        odds = selForm.vars
-        #calculate sums for each row/game (row/game includes three odds (1,x,2))
-        #depending how many odds selected
-        #sum = 1: safe (varma)
-        #sum = 2: partial (osittain vaihdeltu)
-        #sum = 3: full (tukko)
-        for idx, key in enumerate(odds):
-            if idx % 3 == 0:
-                oddsSum = sum(oddsSelected)
-                #number of varmat
-                if oddsSum == 1:
-                    numOfSafes += 1 
-                #number of osittain vaihdellut
-                if oddsSum == 2:
-                    numOfPartial += 1
-                #number of tukkokohde
-                if oddsSum == 3:
-                    numOfFull += 1
-                #clear the list and sums
-                oddsSelected = []
-                numOfSafes = 0
-                numOfPartial = 0
-                numOfFull = 0
-            #is odd selected ?
-            if odds[key] == 'on':
-                oddsSelected.append(1)       
-    
+        #checkboxes from view, dict
+        checkBoxes = selForm.vars
+        #get checkbox values in sorted order 0 -> 38
+        #and replace boolean vales with 0/1
+        for key in sorted(checkBoxes.keys()):
+            if checkBoxes[key]:
+                checkBoxValues.append(1)
+            else:
+                checkBoxValues.append(0)
+        #create checkbox values tuples, 
+        #3 checkbox values per tuple(1,x,2) (=one game)
+        gameOdds = zip(*[iter(checkBoxValues)] * 3)        
+        #calculate the number of checked checkboxes per row/game
+        for idx in range(len(gameOdds)):
+            checkBoxSum = sum(gameOdds[idx])
+            #one checked in a row, safe
+            if checkBoxSum == 1:
+                numOfSafes += 1
+            #two checked in a row, partial
+            if checkBoxSum == 2:
+                numOfPartials += 1
+            #three checked in a row, full
+            if checkBoxSum == 3:
+                numOfFulls += 1
+                
     return dict(rows=rows,
-                selForm=selForm)
+                selForm=selForm,
+                numOfSafes=numOfSafes,
+                numOfPartials=numOfPartials,
+                numOfFulls=numOfFulls)
